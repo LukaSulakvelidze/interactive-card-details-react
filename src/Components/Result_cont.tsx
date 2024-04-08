3ea20e9f9bc80e2f79aab88d259c38a31d1e56c9
Luka Sulakvelidze <luka99258@gmail.com>
2024-04-08 13:56:15 +0400

    Add Check Icon In Result Container

--- src/Components/Result_cont.tsx
+++ src/Components/Result_cont.tsx
@@ -1,9 +1,10 @@
 import Button from "./Button";
+import Check_Icon from "../assets/Icons/Check.png"
 
 const Result_cont = () => {
   return (
     <div className="result_cont">
-      <img className="check_icon" src="./Icons/Check.png" alt="" />
+      <img className="check_icon" src={Check_Icon} alt="Icon" />
       <h2>Thank you!</h2>
       <p>We've added your card details.</p>
       <Button className="continue_button"> Continue</Button>
